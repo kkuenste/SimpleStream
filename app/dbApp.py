4611938d7c959ea4828c0456f9967c835647ca76
Kathryn Janet Kuenster <kkuenste@dsg1.virtual.crc.nd.edu>
2016-10-31 17:30:53 -0400

    Added signup request

--- app/dbApp.py
+++ app/dbApp.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, json
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/main")
 def main():
 	return render_template('index.html')
 
@@ -9,5 +9,18 @@ def main():
 def showSignUp():
 	return render_template('signup.html')
 
+@app.route('/signUp', methods=['POST'])
+def signUp():
+	_fname = request.form['inputFirstName']
+	_lname = request.form['inputLastName']
+	_email = request.form['inputEmail']
+	_password = request.form['inputPassword']
+	
+	if _fname and _lname and _email and _password:
+		return json.dumps({'html':'<span>All fields pass.</span>'})
+	else:
+		return json.dumps({'html':'<span>Enter the required fields</span>'})	
+
+
 if __name__ == "__main__":
-	app.run(host='0.0.0.0')
+	app.run()
